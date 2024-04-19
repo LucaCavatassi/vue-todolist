ca22bde8a2d8f7f47c823ca6c4fd0f6f8e30fb40
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-04-19 15:02:45 +0200

    feat: vue working

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@
 // done = line trough
 // check completed = green mark + circle
 // check uncompleted = red cross + circle
+
+const { createApp } = Vue;
+
+createApp ({
+    data(){
+        return {
+            
+        }
+    },
+    methods: {
+
+    }
+}).mount("#app")
