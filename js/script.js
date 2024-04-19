15a7c08995fbda073a71f3d4bf1d2c028c71e87f
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-04-19 15:25:53 +0200

    feat: toDoList working

--- js/script.js
+++ js/script.js
@@ -8,10 +8,42 @@ const { createApp } = Vue;
 createApp ({
     data(){
         return {
-            
+            allToDo: [
+                {
+                    text: "Quarterly Newsletter",
+                    done: true,
+                },
+                {
+                    text: "Recruiting blog post",
+                    done: false,
+                },
+                {
+                    text: "Mobile App Launch",
+                    done: true,
+                },
+                {
+                    text: "Interview John H",
+                    done: false,
+                },
+                {
+                    text: "Summit update to mobile storefronts",
+                    done: true,
+                },
+                {
+                    text: "Schedule meeting with Alex",
+                    done: false,
+                },
+                {
+                    text: "Recruiting Editorial Calendar",
+                    done: false,
+                },
+            ]
         }
     },
     methods: {
 
     }
 }).mount("#app")
+
+// in class if done = true allora completed else uncompleted 
+// in text if done = true allara &check; else &cross;
